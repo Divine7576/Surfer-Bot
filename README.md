23365a165529f23543300339da7cb63ddf4c36e7
Divine7576 <152605304+Divine7576@users.noreply.github.com>
2023-12-17 12:16:56 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,29 @@
+# DiscordJSv14-Template
+
+It is a simple template to get started with your Discord Bot with Discord.js Library
+
+## Startup
+
+- Create a file named `.env`.
+- Enter the following details to it:
+
+```
+token="YOUR TOKEN"
+MONGODBURL="MONGO DB URI [Only required for commands which requires some data to store]"
+```
+
+- Save the file. Open Terminal in the Project Directory.
+- Run:
+
+```
+npm i
+node .
+```
+
+- You are done.
+
+## Support
+
+Join one of these Discord Servers, to get support:
+
+[<img src="https://discordapp.com/api/guilds/1180197545729851524/widget.png?style=banner2" alt="Discord Banner 1"/>](https://discord.gg/4ZsZSr7jMN)
---
+++ src/functions/handelCommands.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+module.exports = (client) => {
+    client.handleCommands = async (commandFolders, path) => {
+        client.commandArray = [];
+        for (folder of commandFolders) {
+            const commandFiles = fs.readdirSync(`${path}/${folder}`).filter(file => file.endsWith('.js'));
+            for (const file of commandFiles) {
+                const command = require(`../commands/${folder}/${file}`);
+                client.commands.set(command.data.name, command);
+                client.commandArray.push(command.data.toJSON());
+            }
+        }
+
+        client.on("ready", async () => {
+            await client.application.commands.set(client.commandArray);
+        });
+        
+    };
+};
